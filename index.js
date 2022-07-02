8bd9166b73e92ccd89566abfa1ca44db7f65a080
Aryan Singh <101403908+My-website811@users.noreply.github.com>
2022-07-02 13:16:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+
+    if (x === "Arino is back") {
+      window.open("https://www.youtube.com/watch?v=Ki_8UaBaGKU");
+    }
+
+    if (x === "arino is back") {
+      window.open("https://www.youtube.com/watch?v=Ki_8UaBaGKU");
+    }
+
+    if (x === "New trend") {
+      window.open("https://www.youtube.com/shorts/7y3IMMUI_bw");
+    }
+
+    if (x === "new trend") {
+      window.open("https://www.youtube.com/shorts/7y3IMMUI_bw");
+    }
+
+    if (x === "Arino x Death Note") {
+      window.open("https://www.youtube.com/watch?v=4_BgPmZs5h8");
+    }
+
+    if (x === "Arino x death note") {
+      window.open("https://www.youtube.com/watch?v=4_BgPmZs5h8");
+    }
+
+    if (x === "I made a new trend") {
+      window.open("https://www.youtube.com/watch?v=TA7w8l2PzDY");
+    }
+
+    if (x === "i made a new trend") {
+      window.open("https://www.youtube.com/watch?v=TA7w8l2PzDY");
+    }
+
+    if (x === "Bundle now vs Bundle then") {
+      window.open("https://www.youtube.com/shorts/sW5XiX7dQHo");
+    }
+
+    if (x === "bundle now vs bundle then") {
+      window.open("https://www.youtube.com/shorts/sW5XiX7dQHo");
+    }
+
+    if (x === "Bundle now vs bundle then") {
+      window.open("https://www.youtube.com/shorts/sW5XiX7dQHo");
+    }
+
+    if (x === "If free fire was a school") {
+      window.open("https://www.youtube.com/shorts/6BtFQ4wYCg8");
+    }
+
+    if (x === "if free fire was a school") {
+      window.open("https://www.youtube.com/shorts/6BtFQ4wYCg8");
+    }
+
+    if (x == "M500 Challenge") {
+      window.open("https://www.youtube.com/watch?v=sDhbH9lYkwM")
+    }
+
+    if (x == "M500 challenge") {
+      window.open("https://www.youtube.com/watch?v=sDhbH9lYkwM")
+    }
+
+    if (x == "Old generation Vs New generation") {
+      window.open("https://www.youtube.com/shorts/fSm7C8gCZCo")
+    }
+
+    if (x == "Old generation vs new generation") {
+      window.open("https://www.youtube.com/shorts/fSm7C8gCZCo")
+    }
+    
+  }
